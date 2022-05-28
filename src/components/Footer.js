f9c2fb788a1ddb5e79fd68ae51de4121e0cb5e5e
Ed Jasser Guardado <jasserlopezguardado@gmail.com>
2022-05-28 02:45:18 -0500

    addCards

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import './Footer.css'
+import { Container } from '@material-ui/core/container'
 
 const Footer = () => {
   return (
     <div>
        
-        <span className='container'>
-            Make youre site 
-        </span>
+
+       
+
+
         
     </div>
   )
 }
 
 export default Footer
+
+{/* <div>
+         <div className='title-container'>
+            Make youre site 
+        </div>
+        <div className='title-container'>
+            Make youre site 
+        </div>
+       </div>
+        
+        <span className='content-footer'>
+            Make youre site 
+        </span> */}
